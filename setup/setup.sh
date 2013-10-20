b86689d365317505fc6a2cef7b0323e77cbedad3
Josh Earl <jearl@trackabout.com>
2013-10-20 17:01:22 -0400

    Added org-mode journaling function to emacs.

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,49 +8,34 @@ fi
 
 ln -s Dropbox/Programs/Setup/Configuration/Dotfiles/config/.bash_aliases .bash_aliases
 echo Creating link for .bash_aliases ...
+
+if [ -f .emacs ]
+  then rm .emacs 
+fi
+
+ln -s Dropbox/Dotfiles/.emacs .emacs
+echo Creating link for .emacs ...
  
 if [ -f .bash_profile ]
   then rm .bash_profile 
 fi
 
-ln -s Dropbox/Programs/Setup/Configuration/Dotfiles/config/.bash_profile .bash_profile
+ln -s Dropbox/Dotfiles/.bash_profile .bash_profile
 echo Creating link for .bash_profile ...
 
 if [ -f .vimrc ]
   then rm .vimrc 
 fi
 
-ln -s Dropbox/Programs/Setup/Configuration/Dotfiles/config/.vimrc .vimrc
+ln -s Dropbox/Dotfiles/.vimrc .vimrc
 echo Creating link for .vimrc ...
-
-if [ -f .gvimrc ]
-  then rm .gvimrc 
-fi
-
-ln -s Dropbox/Programs/Setup/Configuration/Dotfiles/config/.gvimrc .gvimrc
-echo Creating link for .gvimrc ...
-
-if [ -f .tmux.conf ]
-  then rm .tmux.conf 
-fi
-
-ln -s Dropbox/Programs/Setup/Configuration/Dotfiles/config/.tmux.conf .tmux.conf
-echo creating link for .tmux.conf ...
-
-if [ -f tmux.sh ]
-  then rm tmux.sh 
-fi
-
-# install tmux startup script  
-ln -s Dropbox/Programs/Scripts/Bash/tmux.sh tmux.sh 
-echo creating link for tmux.sh ...
 
 if [ -f update_vim.sh ]
   then rm update_vim.sh 
 fi
 
 # link to vim update script
-ln -s Dropbox/Programs/Setup/Configuration/Dotfiles/update_vim.sh update_vim.sh 
+ln -s Dropbox/Dotfiles/setup/update_vim.sh update_vim.sh 
 echo Creating link for update_vim.sh ...
 
 # install update_plugins script for vim pathogen 
@@ -61,4 +46,4 @@ if [ -f update_plugins.rb ]
 fi
 
 echo Creating link to Pathogen update script ...
-ln -s ~/Dropbox/Programs/Setup/Configuration/Dotfiles/update_plugins.rb update_plugins.rb
+ln -s ~/Dropbox/Dotfiles/setup/update_plugins.rb update_plugins.rb
